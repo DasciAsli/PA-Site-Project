71d6142a036d44793ae4e17e5c6f268b192e6677
DasciAsli <aslihandasci1@gmail.com>
2021-11-28 18:19:57 +0300

    Proje yüklendi

---
+++ PA_BlogProject_2021/Models/ViewModel/IndexViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PA_BlogProject_2021.Models.ViewModel
+{
+   // Home-Index sayfamızda birden fazla tablodan veri çekmemiz gerekiyor
+   //Sayafaya birden fazla veriyi gönderirken kontrolün bir yerden sağlanması için bu sınıf oluşturuldu
+    public class IndexViewModel
+    {
+        public List<Services> Services { get; set; }
+        public List<Blogs> Blogs { get; set; }
+        public List<Sliders> Sliders { get; set; }
+        public List<Contacts> Contacts { get; set; }
+        public List<TeamDescriptions> TeamDescriptions   { get; set; }
+    }
+}
